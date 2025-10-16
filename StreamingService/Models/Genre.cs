3c12297cdd697ba5f32f5e9433d2a10a784e4bbe
KenshinH1mura <githubuser228@gmail.com>
2025-10-16 14:04:25 +0300

    Add files via upload

---
+++ StreamingService/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Genre
+{
+    [Key]
+    public int genre_id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public string age_rating { get; set; }
+
+    public virtual ICollection<Media> Media { get; set; }
+}
---
+++ StreamingService/Models/View.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class View
+{
+    [Key]
+    public int view_id { get; set; }
+    public int user_id { get; set; }
+    public int media_id { get; set; }
+    public DateTime view_date { get; set; }
+    public int watch_time { get; set; }
+    public string device { get; set; }
+
+    [ForeignKey("user_id")]
+    public virtual User User { get; set; }
+    [ForeignKey("media_id")]
+    public virtual Media Media { get; set; }
+}
